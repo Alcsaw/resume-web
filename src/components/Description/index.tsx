e23ec50fd5c079b5abcdf73bde7987defce7e8bd
Alcsaw <alcsaw@hotmail.com>
2020-08-27 08:54:52 -0300

    Changes Description to use Box component; Adds description info;

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 
+import Box from '../Box';
 import './styles.css';
 
 const Description: React.FC = () => {
 
-  const description = `Lifelong learner apaixonado por resolver problemas, principalmente utilizando tecnologia.
-  Trabalhei por 3 anos com administração de sistemas e desenvolvimento web e, atualmente, trabalho com transformação digital com computação em nuvem, desenvolvendo processos ELT e migração de serviços.`
+  const description_ptbr = `Lifelong learner apaixonado por resolver problemas, principalmente utilizando tecnologia.
+  Tenho proatividade e autonomia, o que me possibilita a autoorganizar a rotina de trabalho, ao perceber o que precisa ser feito e ter iniciativa de resolução.
+  Essa atitude, junto de a uma visão sistêmica e organizacional me torna um líder inato, sendo um ponto de referência para colegas de equipe, a quem eu tenho prazer em conseguir ajudar.`
+
+  const description = `Lifelong learner passionate about solving problems, mainly using technology.
+  I have proactivity and autonomy, which allows me to self-organize my work routine, realizing what needs to be done and having a resolution initiative.
+  This attitude, together with a systemic and organizational vision makes me an innate leader, being a point of reference for teammates, whom I am happy to be able to help.`
 
   return (
-    <div className="description">
-      <strong>Resumo</strong>
-      <p className="description-text">{description}</p>
-    </div>
+    <Box label="Resumo">
+      <p className="description-text">{description_ptbr}</p>
+    </Box>
   );
 }
 
