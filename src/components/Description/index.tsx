cf0d23b7d32b7f525b3b60ecc6a220227d43ed80
Alcsaw <alcsaw@hotmail.com>
2020-08-27 17:07:22 -0300

    Margin adjustments; CSS cleaning; typo correction in Description

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -6,7 +6,7 @@ import './styles.css';
 const Description: React.FC = () => {
 
   const description_ptbr = `Lifelong learner apaixonado por resolver problemas, principalmente utilizando tecnologia.
-  Tenho proatividade e autonomia, o que me possibilita a autoorganizar a rotina de trabalho, ao perceber o que precisa ser feito e ter iniciativa de resolução.
+  Tenho proatividade e autonomia, o que me possibilita a autoorganizar a rotina de trabalho, percebendo o que precisa ser feito e, assim, ter iniciativa de resolução.
   Essa atitude, junto de a uma visão sistêmica e organizacional me torna um líder inato, sendo um ponto de referência para colegas de equipe, a quem eu tenho prazer em conseguir ajudar.`
 
   const description = `Lifelong learner passionate about solving problems, mainly using technology.
