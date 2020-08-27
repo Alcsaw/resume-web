c6cd53732cdbcac1e77bd2fbc9dac10a6ff65639
Alcsaw <alcsaw@hotmail.com>
2020-08-27 17:08:34 -0300

    Adds Education componenet

---
+++ src/components/Education/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Box from '../Box';
+
+import './styles.css';
+
+const Education: React.FC = () => {
+
+  const certificationName = "Bacharel em Ciência da Computação";
+  const issuerName = "Unisc";
+  const issuerLink = "unisc.br";
+  const certificationGrade = "GPA: 9.05";
+
+  return (
+    <Box label="Education">
+      <ul className="certifications-list">
+        <li className="certification-item">
+          <div className="item-name">
+            <span className="certification-name">{certificationName}</span>
+            <a className="issuer-link" href={issuerLink}>{issuerName}</a>
+          </div>
+          <div className="certification-grade">{certificationGrade}</div>
+        </li>
+      </ul>
+    </Box>
+  );
+}
+
+export default Education;
--- src/pages/Resume/index.tsx
+++ src/pages/Resume/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from '../../components/Header';
 import Description from '../../components/Description';
 import Experiences from '../../components/Experiences';
+import Education from '../../components/Education';
 
 import './styles.css';
 
@@ -12,6 +13,9 @@ export default function Resume() {
       <Header />
       <Description />
       <Experiences />
+      <Education />
+
+      <p className="footer-note">Design and code made with <span role="img" aria-label="love">❤️</span> by <a href="https://github.com/Alcsaw/resume-web">Alcsaw</a></p>
     </div>
 
   );
